2bca322fb4db4b99a3a80fbff66d43e4874e3240
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-12-16 21:33:36 +0530

    commit routing demo app into the repo

---
+++ apps/day-08/store-app-routing/src/app/products/product-detail/product-detail.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { ProductModel } from '../product.model';
+import { ProductsService } from '../products.service';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  private id: string;
+  product: ProductModel;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: ProductsService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((map) => {
+      this.id = map.get('id');
+      this.product = this.service.getProduct(this.id);
+    });
+  }
+
+  onEdit() {
+    this.router.navigate(['/products', this.id, 'edit']);
+  }
+
+  onDelete() {
+    if (confirm('Are you sure?')) {
+      this.service.deleteProduct(this.product.id);
+      this.router.navigate(['/products']);
+    }
+  }
+
+}
