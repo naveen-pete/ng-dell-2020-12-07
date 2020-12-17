1a004133808c05f3b4a64184743abbc11caa7f9d
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-12-17 20:01:59 +0530

    commit store app for day 9

---
+++ apps/day-09/store-app/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import { SignUpComponent } from "./auth/sign-up/sign-up.component";
+import { LoginComponent } from "./auth/login/login.component";
+
+import { ProductsComponent } from './products/products.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ProductFormComponent } from './products/product-form/product-form.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+
+    HomeComponent,
+    NotFoundComponent,
+
+    SignUpComponent,
+    LoginComponent,
+
+    ProductsComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ apps/day-09/store-app/src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
+import { ProductModel } from '../product.model';
+import { ProductsService } from '../products.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, OnDestroy {
+  products: ProductModel[] = [];
+  private subRefreshProducts: Subscription;
+
+  constructor(
+    private router: Router,
+    private service: ProductsService,
+  ) { }
+
+  ngOnInit() {
+    this.products = this.service.getAllProducts();
+
+    this.subRefreshProducts = this.service.refreshProducts.subscribe(
+      (products: ProductModel[]) => this.products = products
+    );
+  }
+
+  onAdd() {
+    this.router.navigate(['/products/new']);
+  }
+
+  ngOnDestroy() {
+    if (this.subRefreshProducts) {
+      this.subRefreshProducts.unsubscribe();
+    }
+  }
+}
